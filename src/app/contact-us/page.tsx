8ded3096237d26c0e8b38f0d7d6ff53b55f89a6a
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-06-12 16:53:52 -0700

    Started on form for Contact Us form

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,7 +1,18 @@
 export default function ContactUs() {
   return (
-    <div>
-      <p>im a form</p>
-    </div>
+    <section className="text-center">
+      <div className="w-4/5 m-auto md:w-3/5">
+        <h2 className="text-4xl">Contact Us</h2>
+        <p className="text-2xl">
+          I wouldn&apos;t be here without all of your support! Send me a
+          questions or suggestion with the form below!
+        </p>
+        <form className="flex flex-col">
+          <input type="name" placeholder="Name"></input>
+          <input type="email" placeholder="Email"></input>
+          <textarea placeholder="Message"></textarea>
+        </form>
+      </div>
+    </section>
   );
 }
