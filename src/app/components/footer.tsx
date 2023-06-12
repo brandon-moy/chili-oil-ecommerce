f04bede926a8989653c0eb334ecf14dd1447fe85
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-06-12 15:12:17 -0700

    Completed base layout for Footer component

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,7 +1,18 @@
 export default function Footer() {
   return (
-    <div>
-      <h1>FOOTER STUFF</h1>
+    <div className="bg-maroon">
+      <div className="px-4 py-6">
+        <h1 className="text-xl">Contact Me</h1>
+        <p className="py-4">
+          <a className="text-sm" href="mailto:brandon@brandonmoy.com">
+            brandon@brandonmoy.com
+          </a>
+        </p>
+        {/* {Fix contact form text to link after creating page} */}
+        <p className="text-sm">
+          Send me a message via my <a className="underline">contact form!</a>
+        </p>
+      </div>
     </div>
   );
 }
