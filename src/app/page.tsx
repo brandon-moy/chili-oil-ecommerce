d32d4c5120774c10a6562cfdf32b1beaf3d16591
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-06-12 15:31:29 -0700

    Centered SOME TEXT on hero image -- to change text at later time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,9 @@ export default function Home() {
     <main className="flex flex-col items-center justify-between min-h-screen">
       <div className="relative w-full h-screen">
         <div className="w-full h-full bg-[url('/chili_stock.webp')] bg-cover bg-center brightness-[65%] contrast-150"></div>
+        <p className="absolute text-6xl -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+          SOME TEXT
+        </p>
       </div>
     </main>
   );
