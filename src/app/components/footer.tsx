a938dee515389fd802db3313b8486ba0a34b6657
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-06-12 15:26:15 -0700

    Added sizing for Footer on Desktep, set up hero image on home page

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,15 +1,18 @@
 export default function Footer() {
   return (
     <div className="bg-maroon">
-      <div className="px-4 py-6">
-        <h1 className="text-xl">Contact Me</h1>
-        <p className="py-4">
-          <a className="text-sm" href="mailto:brandon@brandonmoy.com">
+      <div className="px-4 py-6 md:py-16 md:px-12">
+        <h1 className="text-xl md:text-3xl">Contact Me</h1>
+        <p className="py-4 md:py-8">
+          <a
+            className="text-sm md:text-lg"
+            href="mailto:brandon@brandonmoy.com"
+          >
             brandon@brandonmoy.com
           </a>
         </p>
         {/* {Fix contact form text to link after creating page} */}
-        <p className="text-sm">
+        <p className="text-sm md:text-lg">
           Send me a message via my <a className="underline">contact form!</a>
         </p>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,10 @@ import prisma from "../../lib/prisma";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen"></main>
+    <main className="flex flex-col items-center justify-between min-h-screen">
+      <div className="relative w-full h-screen">
+        <div className="w-full h-full bg-[url('/chili_stock.webp')] bg-cover bg-center brightness-[65%] contrast-150"></div>
+      </div>
+    </main>
   );
 }
