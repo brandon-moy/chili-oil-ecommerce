84e4bd364ba8bb36440ec1209d1bbfb9d19cdab8
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-06-13 12:24:13 -0700

    Removed form elements - no hydration issue - working to fix form

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,32 +1,14 @@
 export default function ContactUs() {
   return (
-    <section className="text-center">
-      <div className="w-11/12 m-auto pt-36 md:w-3/5">
+    <>
+      <div className="w-11/12 m-auto text-center pt-36 md:w-3/5">
         <h2 className="text-2xl md:text-4xl">Contact Us</h2>
         <p className="text-md md:text-2xl">
           I wouldn&apos;t be here without all of your support! Send me a
           questions or suggestion with the form below!
         </p>
-        <form className="flex flex-col">
-          <input
-            name="name"
-            type="name"
-            className="p-1 my-1 bg-gray-100 rounded md:h-12 md:my-4"
-            placeholder="Name"
-          ></input>
-          <input
-            name="email"
-            type="email"
-            className="p-1 my-1 bg-gray-100 rounded md:h-12 md:my-4"
-            placeholder="Email"
-          ></input>
-          <textarea
-            name="message"
-            className="p-1 mt-1 mb-6 bg-gray-100 rounded md:mt-4 md:mb-12 md:h-48 h-28"
-            placeholder="Message"
-          ></textarea>
-        </form>
+        <form className="flex flex-col"></form>
       </div>
-    </section>
+    </>
   );
 }
