9767e7caa0839d4a5c413c9f0a8f82cd98b888e1
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-06-13 11:52:33 -0700

    Completed form layout for contact us page, working on fixing hydration issue on page

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,16 +1,30 @@
 export default function ContactUs() {
   return (
     <section className="text-center">
-      <div className="w-4/5 m-auto md:w-3/5">
-        <h2 className="text-4xl">Contact Us</h2>
-        <p className="text-2xl">
+      <div className="w-11/12 m-auto pt-36 md:w-3/5">
+        <h2 className="text-2xl md:text-4xl">Contact Us</h2>
+        <p className="text-md md:text-2xl">
           I wouldn&apos;t be here without all of your support! Send me a
           questions or suggestion with the form below!
         </p>
         <form className="flex flex-col">
-          <input type="name" placeholder="Name"></input>
-          <input type="email" placeholder="Email"></input>
-          <textarea placeholder="Message"></textarea>
+          <input
+            name="name"
+            type="name"
+            className="p-1 my-1 bg-gray-100 rounded md:h-12 md:my-4"
+            placeholder="Name"
+          ></input>
+          <input
+            name="email"
+            type="email"
+            className="p-1 my-1 bg-gray-100 rounded md:h-12 md:my-4"
+            placeholder="Email"
+          ></input>
+          <textarea
+            name="message"
+            className="p-1 mt-1 mb-6 bg-gray-100 rounded md:mt-4 md:mb-12 md:h-48 h-28"
+            placeholder="Message"
+          ></textarea>
         </form>
       </div>
     </section>
